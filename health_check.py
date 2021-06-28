76ff5ee455abc4b89b660f40fea3f71039ea7fb0
Arun Kumar Singh <singh.arunk16@hotmail.com>
2021-06-28 16:56:13 +0100

    Update health_check.py

--- health_check.py
+++ health_check.py
@@ -7,6 +7,7 @@ i = 0
 while i in range(100):
     time.sleep(1)
     results_loop = requests.get("http://localhost:9200/_cluster/health",headers={'Metadata': 'true'},)
+    #results_loop = requests.get("https://localhost:9201/_cluster/health",headers={'Authorization': 'Basic xxxxbase64-encxxxx'},verify=False)
     jdata = json.loads(results_loop.text)
     els_name = jdata["cluster_name"]
     els_health = jdata["status"]
